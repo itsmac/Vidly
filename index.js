18ac43e464ab69dcda03b3041eace2f3461d8bbc
itsmac <saagarmac@gmail.com>
2023-01-29 23:05:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+const genreList = [
+  {
+    id: 1,
+    genretype: "action",
+  },
+  {
+    id: 2,
+    genretype: "thriller",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hellowwww Worldddd !!!!");
+});
+
+app.get("/api/v1/genres", (req, res) => {
+  res.send(genreList);
+});
+
+app.post("/api/v1/genres", (req, res) => {
+    let newGenre = {
+        id : genreList.length + 1,
+        genretype : req.body.genretype
+    }
+    genreList.push(newGenre);
+    res.send(newGenre);
+});
+
+app.put("/api/v1/genres/:id",(req,res) => {
+    let genre = genreList.find(gen => gen.id === parseInt(req.params.id));
+    if(!genre){
+        return res.status(404).send("The is not found");
+    }
+    else{
+        genre.genretype = req.body.genretype;
+        res.send(genre);
+    }
+
+});
+// app.delete();
+
+app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));

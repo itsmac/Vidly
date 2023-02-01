84adb535f2fb025d406cfad2420a4434c57a5229
itsmac <saagarmac@gmail.com>
2023-02-01 11:57:25 +0530

    feat : added input validation

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const Joi = require("joi");
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
+
+function isValid(req) {
+  const schema = Joi.object({
+    genretype: Joi.string().min(3).required(),
+  });
+
+  const { value, error } = schema.validate(req.body);
+  return error;
+}
 
 const genreList = [
   {
@@ -24,36 +34,40 @@ app.get("/api/v1/genres", (req, res) => {
 });
 
 app.post("/api/v1/genres", (req, res) => {
-    let newGenre = {
-        id : genreList.length + 1,
-        genretype : req.body.genretype
-    }
-    genreList.push(newGenre);
-    res.send(newGenre);
+  const error = isValid(req);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  let newGenre = {
+    id: genreList.length + 1,
+    genretype: req.body.genretype,
+  };
+  genreList.push(newGenre);
+  res.send(newGenre);
 });
 
-app.put("/api/v1/genres/:id",(req,res) => {
-    let genre = genreList.find(gen => gen.id === parseInt(req.params.id));
-    if(!genre){
-        return res.status(404).send("The is not found");
+app.put("/api/v1/genres/:id", (req, res) => {
+  let genre = genreList.find((gen) => gen.id === parseInt(req.params.id));
+  if (!genre) {
+    return res.status(404).send("The is not found");
+  } else {
+    const error = isValid(req);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
     }
-    else{
-        genre.genretype = req.body.genretype;
-        res.send(genre);
-    }
-
-});
-
-app.delete("/api/v1/genres/:id", (req,res) => {
-    let genre = genreList.find(gen => gen.id === parseInt(req.params.id))
-    if(!genre){
-        return res.status(404).send("This id is not found !!");
-    }
-    const index = genreList.indexOf(genre);
-    genreList.splice(index,1);
+    genre.genretype = req.body.genretype;
     res.send(genre);
+  }
 });
 
-
+app.delete("/api/v1/genres/:id", (req, res) => {
+  let genre = genreList.find((gen) => gen.id === parseInt(req.params.id));
+  if (!genre) {
+    return res.status(404).send("This id is not found !!");
+  }
+  const index = genreList.indexOf(genre);
+  genreList.splice(index, 1);
+  res.send(genre);
+});
 
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));

f053e312506fa49680bb7b23c403e26777670a85
itsmac <saagarmac@gmail.com>
2023-03-27 12:40:35 +0530

    feat : mongo connection get and post implemented

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const Joi = require("joi");
 const app = express();
 const genres = require("./routes/genres");
 const defaultPage = require("./routes/default");
+const { default: mongoose } = require("mongoose");
 const PORT = 4000;
 
-// app.use(express.json()); didn't work well with current code need to check other middlewares
+app.use(express.json()); //didn't work well with current code need to check other middlewares
+
+mongoose.connect('mongodb://127.0.0.1/vidly')
+        .then(()=> console.log("Connection successul"))
+        .catch( err => console.log("Connection failed "+ err));
+
+
 
 app.use('/api/v1/genres', genres);
 app.use('/',defaultPage);
--- routes/genres.js
+++ routes/genres.js
@@ -1,68 +1,83 @@
 const express = require('express');
 const router = express.Router();
-
+const mongoose = require('mongoose');
+const Joi = require('joi')
 
 function isValid(req) {
-    const schema = Joi.object({
-      genretype: Joi.string().min(3).required(),
-    });
-  
-    const { value, error } = schema.validate(req.body);
-    return error;
-  }
-  
-  const genreList = [
-    {
-      id: 1,
-      genretype: "action",
-    },
-    {
-      id: 2,
-      genretype: "thriller",
-    },
-  ];
-  
-
-  
-  router.get("/", (req, res) => {
-    res.send(genreList);
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
   });
-  
-  router.post("/", (req, res) => {
+
+  const { value, error } = schema.validate(req.body);
+  return error;
+}
+
+//creating Schema (to define the shape of the document)
+const GenreSchema = new mongoose.Schema({
+  name: { 
+    type: String,
+    maxlength: 10,
+    minlenght: 3, 
+    required: true },
+
+});
+
+//Creating the model
+const Genre = mongoose.model('Genre', GenreSchema); 
+
+// const genreList = [
+//   {
+//     id: 1,
+//     genretype: "action",
+//   },
+//   {
+//     id: 2,
+//     genretype: "thriller",
+//   },
+// ];
+
+
+
+router.get("/", async (req, res) => {
+  const genreList = await Genre.find();
+  res.send(genreList);
+});
+
+router.post("/", async (req, res) => {
+  const error = isValid(req);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  console.log(req);
+  let newGenre = new Genre({
+    name : req.body.name,
+  });
+  const result = await newGenre.save();
+  res.send(result);
+});
+
+router.put("/:id", (req, res) => {
+  let genre = genreList.find((gen) => gen.id === parseInt(req.params.id));
+  if (!genre) {
+    return res.status(404).send("The is not found");
+  } else {
     const error = isValid(req);
     if (error) {
       return res.status(400).send(error.details[0].message);
     }
-    let newGenre = {
-      id: genreList.length + 1,
-      genretype: req.body.genretype,
-    };
-    genreList.push(newGenre);
-    res.send(newGenre);
-  });
-  
-  router.put("/:id", (req, res) => {
-    let genre = genreList.find((gen) => gen.id === parseInt(req.params.id));
-    if (!genre) {
-      return res.status(404).send("The is not found");
-    } else {
-      const error = isValid(req);
-      if (error) {
-        return res.status(400).send(error.details[0].message);
-      }
-      genre.genretype = req.body.genretype;
-      res.send(genre);
-    }
-  });
-  
-  router.delete("/:id", (req, res) => {
-    let genre = genreList.find((gen) => gen.id === parseInt(req.params.id));
-    if (!genre) {
-      return res.status(404).send("This id is not found !!");
-    }
-    const index = genreList.indexOf(genre);
-    genreList.splice(index, 1);
+    genre.genretype = req.body.genretype;
     res.send(genre);
-  });
+  }
+});
 
-  module.exports = router;
+router.delete("/:id", (req, res) => {
+  let genre = genreList.find((gen) => gen.id === parseInt(req.params.id));
+  if (!genre) {
+    return res.status(404).send("This id is not found !!");
+  }
+  const index = genreList.indexOf(genre);
+  genreList.splice(index, 1);
+  res.send(genre);
+});
+
+module.exports = router;
